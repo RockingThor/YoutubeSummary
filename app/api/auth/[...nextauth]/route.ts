8b808913cfedc3386c4ac447782d14f3d5412b67
Rohit Nandi <rohitnandibmp@gmail.com>
2023-11-16 16:38:56 +0530

    after prisma schema and some modification in the file structure

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,45 @@
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const handler = NextAuth({
+  secret: process.env.NEXT_AUTH_SECRET,
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  adapter: PrismaAdapter(prisma),
+  session: { strategy: "jwt" },
+  cookies: {
+    sessionToken: {
+      name: "next-auth.session-token",
+      options: {
+        httpOnly: true,
+        sameSite: "lax",
+        path: "/",
+        //localhost==>(https://stackoverflow.com/a/1188145)
+        maxAge: 60 * 60 * 24 * 30,
+        domain: undefined,
+        secure: process.env.NODE_ENV === "production",
+      },
+    },
+  },
+  callbacks: {
+    jwt: async ({ token, user }) => {
+      if (!token.email) {
+        return {};
+      }
+      if (user) {
+        token.user = user;
+      }
+      return token;
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
